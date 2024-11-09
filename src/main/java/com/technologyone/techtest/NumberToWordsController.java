5f05a2afb6c3c017474a62cb45ae377534f50bc8
millsmi <mike@themills.id.au>
2024-11-09 13:10:38 +1000

    Added simple web page to take input and output the result.

---
+++ src/main/java/com/technologyone/techtest/NumberToWordsController.java
@@ -0,0 +1,26 @@
+package com.technologyone.techtest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class NumberToWordsController {
+
+
+    @GetMapping("/input")
+    public String showInputForm() {
+        return "numberToWords";
+    }
+
+    @PostMapping("/submit")
+    public String handleInput(@RequestParam("userInput") String userInput, Model model) {
+        // Pass the input to the model to display it back on the result page
+        model.addAttribute("userInput", userInput);
+        return "numberToWords";
+    }
+
+}
