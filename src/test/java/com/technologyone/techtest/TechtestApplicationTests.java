2cc6da9c6fcd081c3b8e44e26961e9eb7f726670
millsmi <mike@themills.id.au>
2024-11-09 12:30:49 +1000

    Initial spring intilizer template

---
+++ src/test/java/com/technologyone/techtest/TechtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technologyone.techtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
